99a1c4a990381cae0c3eb22d32975477a343b6fc
eyosdesta <desta.eyosias@gmail.com>
2024-01-23 07:31:57 -0500

    render loop removed

--- QTable_selflearning.py
+++ QTable_selflearning.py
@@ -34,23 +34,3 @@ def main():
             
 
     
-    done = False
-    reward = 0
-    penality = 0
-    count_steps = 0
-    rewards = 0
-
-    while not done:
-        take_action = env.step(action)
-        state = take_action[0]
-        reward = take_action[1]
-        done = take_action[2]
-        if (reward == -10):
-            penality += 1
-        rewards = rewards + reward
-        count_steps += 1
-
-    env.render()
-    print("Steps: ", steps)
-    print("Penality: ", penality)
-    print("reward: ", reward)
